b30a763fba37ef4f510bf9b0e6e7bd5343c5e8cb
Salman <salman.ykhan@hotmail.co.uk>
2023-06-19 13:44:18 +1000

    Added subtract.java program

---
+++ src/samplePackage/Subtract.java
@@ -0,0 +1,16 @@
+package samplePackage;
+
+public class Subtract {
+
+	public static void main(String[] args) {
+		int first = 100;
+		int second = 80;
+		
+		// Add two numbers
+		
+		int sum = first - second;
+		System.out.println(first + " + " + second + " = " + sum);
+
+	}
+
+}
